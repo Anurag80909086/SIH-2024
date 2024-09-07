da77329fa952a645be36819a087b94e4bf57fbd8
Anurag80909086 <anurag.singh26@amity.edu>
2024-09-07 12:28:56 +0530

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import ImageSwiper from "./components/image-swiper/ImageSwiper";
+// import ImageSwiper from "./components/image-swiper/ImageSwiper";
 
 import "./App.css";
 const App = () => {
