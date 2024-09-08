b7b42be0f3bf76fcfc4e036d539a59821f66ca33
Anurag80909086 <anurag.singh26@amity.edu>
2024-09-08 20:39:55 +0530

    e4

--- src/components/image-swiper/imageSwiper.jsx
+++ src/components/image-swiper/imageSwiper.jsx
@@ -1,75 +1,71 @@
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
-// import "./ImageSwiper.css";
-// import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./ImageSwiper.css";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
-// import poster1 from "./images/poster1.jpg";
-// import poster2 from "./images/poster2.jpg";
-// import poster3 from "./images/poster3.jpg";
-// import poster4 from "./images/poster4.avif";
-// import poster5 from "./images/poster5.webp";
-// import poster6 from "./images/poster6.jpg";
-// import poster7 from "./images/poster7.webp";
-// import poster8 from "./images/poster8.webp";
-// import poster9 from "./images/poster9.jpg";
-// import poster10 from "./images/poster10.jpg";
-// function ImageSwiper() {
-//   return (
-//     <>
-//       <Swiper
-//         spaceBetween={0}
-//         centeredSlides={true}
-//         autoplay={{
-//           delay: 2500,
-//           disableOnInteraction: false,
-//         }}
-//         pagination={{
-//           clickable: true,
-//         }}
-//         grabCursor={true}
-//         navigation={true}
-//         modules={[Autoplay, Pagination, Navigation]}
-//         className="mySwiper"
-//       >
-//         <SwiperSlide>
-//           <img src={poster1} alt="poster" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={poster2} alt="poster" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={poster3} alt="poster" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={poster4} alt="poster" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={poster5} alt="poster" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={poster6} alt="poster" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={poster7} alt="poster" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={poster8} alt="poster" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={poster9} alt="poster" />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={poster10} alt="poster" />
-//         </SwiperSlide>
-//       </Swiper>
-//     </>
-//   );
-// }
-
-const ImageSwiper = () => {
-  return <div>ImageSwiper</div>;
-};
+import poster1 from "./images/poster1.jpg";
+import poster2 from "./images/poster2.jpg";
+import poster3 from "./images/poster3.jpg";
+import poster4 from "./images/poster4.avif";
+import poster5 from "./images/poster5.webp";
+import poster6 from "./images/poster6.jpg";
+import poster7 from "./images/poster7.webp";
+import poster8 from "./images/poster8.webp";
+import poster9 from "./images/poster9.jpg";
+import poster10 from "./images/poster10.jpg";
+function ImageSwiper() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={0}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        grabCursor={true}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={poster1} alt="poster" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={poster2} alt="poster" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={poster3} alt="poster" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={poster4} alt="poster" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={poster5} alt="poster" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={poster6} alt="poster" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={poster7} alt="poster" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={poster8} alt="poster" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={poster9} alt="poster" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={poster10} alt="poster" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
 
 export default ImageSwiper;
