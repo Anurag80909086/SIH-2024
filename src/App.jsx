70879518be2047b9055571f46934f2a9c31aa53f
Anurag80909086 <anurag.singh26@amity.edu>
2024-09-08 20:53:48 +0530

    e9

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 // import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-// import ImageSwiper from "./components/image-swiper/ImageSwiper";
+import ImageSwiper from "./components/image-swiper/ImageSwiper";
 import "./App.css";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      {/* <ImageSwiper /> */}
+      <ImageSwiper />
       <Footer />
     </>
   );
