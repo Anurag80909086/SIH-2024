533d222b1f3638ad991d2203599d851bf631742d
Anurag80909086 <anurag.singh26@amity.edu>
2024-09-10 10:48:53 +0530

    added 360

--- src/components/cityCard/CityCard.jsx
+++ src/components/cityCard/CityCard.jsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
 import "./CityCard.css";
 const CityCard = (props) => {
   const { url, name, cityDes } = props;
+  const [isOpen, setIsopen] = useState(false);
+  function open360() {
+    setIsopen(true);
+  }
+  function close360() {
+    setIsopen(false);
+  }
   return (
-    <div className="cityCard">
-      <img src={url} alt="city" />
-      <div className="name">
-        <span>{name}</span>
-        <p>{cityDes}</p>
+    <>
+      <div className="cityCard" title="click to view 360deg" onClick={open360}>
+        <img src={url} alt="city" />
+        <div className="name">
+          <span>{name}</span>
+          <p>{cityDes}</p>
+        </div>
       </div>
-    </div>
+      {isOpen ? (
+        <div className="container360">
+          <iframe
+            title="Taj Mahal"
+            width="100%"
+            height="100%"
+            src="https://www.airpano.com/embed.php?3D=taj-mahal-india"
+            allowFullScreen
+          ></iframe>
+          <i
+            onClick={close360}
+            className="fa-solid fa-circle-xmark"
+            id="closeBtn"
+          ></i>
+        </div>
+      ) : null}
+    </>
   );
 };
 
